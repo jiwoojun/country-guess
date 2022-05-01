0c82220461a6dab26d2077135bc9f8794ec1677a
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-05-01 16:20:29 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,25 +19,71 @@ let country_list = [
 "russia",
 "sweden",
 "thailand",
-"america",
+"usa",
 "vietnam",
 "yemen",
-"zimbabwe"
+"zimbabwe",
+"newzealand",
+"spain",
+"turkey",
+"peru",
+"portugal",
+"srilanka",
+"madagascar",
+"papuanewguinea",
+"tonga",
+"fiji",
+"indonesia",
+"india",
+"argentina",
+"phillipines",
+"ukraine",
+"vaticancity",
+"myammar",
+"laos",
+"tuvalu",
+"vanuatu",
+"venezuela",
+"paraguay",
+"guyana",
+"poland",
+"kyrgyzstan",
+"northkorea",
+"southkorea",
+"micronesia",
+"algeria",
+"chile",
+"colombia",
+"cuba",
+"bangladesh",
+"china",
+"uae",
+"turkmenistan",
+"iran",
+"iraq",
+"saudiarabia",
+"uk",
+"ireland",
+"norway",
+"southafrica",
+"zambia",
+"lesotho",
+"malawi"
 ]
 story.printText("guess the country", 80, 100)
 let abc_guess = game.askForString("type in 10 letters (no numbers)", 10)
 story.printText(abc_guess, 80, 35)
-let random_country = randint(0, 23)
+let random_country = randint(0, 69)
 let correct_guess_count = 0
 let correct_abc = ""
 for (let abcindex = 0; abcindex <= 9; abcindex++) {
     if (country_list[random_country].includes(abc_guess.charAt(abcindex))) {
         correct_guess_count += 1
-        correct_abc = "" + correct_abc + abc_guess.charAt(abcindex)
+        correct_abc = 0
     }
 }
 game.showLongText(correct_abc, DialogLayout.Top)
-let guessed_country = game.askForString("type in 10 letters (no numbers)", 10)
+let guessed_country = game.askForString("type in 10 letters (no numbers)", 14)
 if (guessed_country == country_list[random_country]) {
     game.splash("you guessed the country!")
     game.over(true)
