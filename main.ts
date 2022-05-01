b56ac5dec16989dff81bcc036e2762a551179251
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-05-01 18:31:56 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -76,13 +76,16 @@ story.printText(abc_guess, 80, 35)
 let random_country = randint(0, 69)
 let correct_guess_count = 0
 let correct_abc = ""
+let incorrect_abc = ""
 for (let abcindex = 0; abcindex <= 9; abcindex++) {
     if (country_list[random_country].includes(abc_guess.charAt(abcindex))) {
         correct_guess_count += 1
         correct_abc = "" + correct_abc + abc_guess.charAt(abcindex)
+    } else {
+        incorrect_abc = "" + incorrect_abc + abc_guess.charAt(abcindex)
     }
 }
-game.showLongText(correct_abc, DialogLayout.Top)
+game.showLongText("Correct:" + correct_abc + "       " + "Incorrect:" + incorrect_abc, DialogLayout.Top)
 let guessed_country = game.askForString("try to guess the country", 14)
 if (guessed_country == country_list[random_country]) {
     game.splash("you guessed the country!")
