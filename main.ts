24f2116cac2b137cb6201d76ff9f270ccb201fa7
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-05-01 15:07:40 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,4 +25,14 @@ let country_list = [
 "zimbabwe"
 ]
 story.printText("guess the country", 80, 100)
-game.splash("" + country_list[randint(0, 23)])
+let abc_guess = game.askForString("type in 10 letters (no numbers)", 10)
+story.printText(abc_guess, 80, 35)
+let random_country = randint(0, 23)
+game.splash("" + country_list[random_country])
+let correct_guess_count = 0
+for (let abcindex = 0; abcindex <= 9; abcindex++) {
+    if (country_list[random_country].includes(abc_guess.charAt(abcindex))) {
+        correct_guess_count += 1
+        info.setScore(correct_guess_count)
+    }
+}
