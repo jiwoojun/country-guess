db8e9606c66dd3520d41d57d0af0703d963deb5a
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2022-05-01 14:00:18 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let country_list = [
+"austrailia",
+"brazil",
+"canada",
+"dennmark",
+"egypt",
+"finland",
+"germany",
+"hungary",
+"iceland",
+"japan",
+"kuwait",
+"luxenbourg",
+"mongolia",
+"nepal",
+"oman",
+"pakistan",
+"qatar",
+"russia",
+"sweden",
+"thailand",
+"united states of america",
+"vietnam",
+"yemen",
+"zimbabwe"
+]
+story.printText("guess the country", 80, 100)
+game.splash("" + country_list[randint(0, 23)])
